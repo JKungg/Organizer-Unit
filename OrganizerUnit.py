6f70d66f859e8f8e41deef6fca2c0e1b83670b6f
kungfu8 <jackdilkens@gmail.com>
2020-02-14 23:50:39 -0500

    Database Complete, now works in multiple server :)

--- OrganizerUnit.py
+++ OrganizerUnit.py
@@ -28,33 +28,50 @@ cursor = db.cursor()
 @client.command()
 @commands.has_permissions(administrator = True)
 async def setchannel(ctx, arg1):
-    global matchChannel
     await client.wait_until_ready()
     arg1 = int(arg1)
-
+    guildID = ctx.guild.id
     if isinstance(arg1, int):
         matchChannel = client.get_channel(arg1)
+        mchannel = str(matchChannel)
 
+        print("Now Inserting Channel ID INTO Database...")
+        sql_update_channelID = (f'''UPDATE main
+            SET channel_id = ?
+            WHERE guild_id = ?
+            ''')
+        info = (mchannel, guildID)
+        cursor.execute(sql_update_channelID, info)
+        db.commit()
+        print("Channel Added to Database.")
+        
         try:
             await matchChannel.send("Match Channel Set!")
         except:
             await ctx.send("Invalid Channel ID, Check if you wrote it correctly!")
-
-        mchannel = str(matchChannel)
-        cursor.execute('INSERT INTO main(matchChannel) values(?)',
-        (mchannel,))
-        db.commit()
+        
     else:
         await ctx.send("Invalid Channel ID; Right click the channel and click **'Copy ID'**!")
+
+
+
 
 @client.command()
 @commands.has_permissions(administrator = True)
 async def setteam(ctx, *args):
-    global team
     await client.wait_until_ready()
-
     team = ' '.join(args)
-    print(team)
+    guildID = ctx.guild.id
+
+    sql_team_set = (f"""UPDATE main
+        SET team = ?
+        WHERE guild_id = ?
+        """)
+    info = (team, guildID)
+    cursor.execute(sql_team_set, info)
+    db.commit()
+    print("Team Added to Datebase")
+
     await ctx.send(f"You have set your team to : **{team}**")
 
 
@@ -64,6 +81,15 @@ async def setteam(ctx, *args):
 # .scrim command sending to match channel
 @client.command()
 async def setscrim(ctx, arg1, arg2):
+
+    cursor.execute(f"""SELECT channel_id FROM main WHERE guild_id = {ctx.guild.id}""")
+    channelid = cursor.fetchone()
+    channelid = channelid[0]
+
+
+
+    matchChannel = discord.utils.get(ctx.guild.text_channels, name=f'{channelid}')
+
     await matchChannel.send(f"Scrim Scheduled for: **{arg1.capitalize()} at {arg2.capitalize()}**")
     await ctx.message.delete()
 
@@ -72,6 +98,12 @@ async def setscrim(ctx, arg1, arg2):
 
 @client.command()
 async def setmatch(ctx, arg1, arg2):
+
+    cursor.execute(f"""SELECT channel_id FROM main WHERE guild_id = {ctx.guild.id}""")
+    channelid = cursor.fetchone()
+    channelid = channelid[0]
+
+    matchChannel = discord.utils.get(ctx.guild.text_channels, name=f'{channelid}')
     await matchChannel.send(f"League Match Scheduled for: **{arg1.capitalize()} at {arg2.capitalize()}**")
     await ctx.message.delete()
 
@@ -79,6 +111,12 @@ async def setmatch(ctx, arg1, arg2):
 # TODO: Make it get .scrim time and remind 15 minutes before!
 @client.command()
 async def sremind(ctx, arg1, arg2):
+
+    cursor.execute(f"""SELECT channel_id FROM main WHERE guild_id = {ctx.guild.id}""")
+    channelid = cursor.fetchone()
+    channelid = channelid[0]
+
+    matchChannel = discord.utils.get(ctx.guild.text_channels, name=f'{channelid}')
     await matchChannel.send(f"```Reminder you have a scrim in >>> {arg1} {arg2}```")
     await ctx.message.delete()
 
@@ -89,7 +127,7 @@ def getTeams():
     page = requests.get("https://vrmasterleague.com/EchoArena/Matches/")
     soup = BeautifulSoup(page.content, 'html.parser')
     matchNode = soup.find(id='MatchesScheduled_MatchesNode')
-    matchesHidden = matchNode.find_all(class_ = 'rows-hider-body rows-hider-hidden')
+    # matchesHidden = matchNode.find_all(class_ = 'rows-hider-body rows-hider-hidden')
     orangeCell = matchNode.find_all(class_ = 'home_team_cell')
     blueCell = matchNode.find_all(class_ = 'away_team_cell')
 
@@ -110,12 +148,24 @@ def getTeams():
 async def sendmatch(ctx):
 
     await client.wait_until_ready()
+    
+    cursor.execute(f"""SELECT channel_id FROM main WHERE guild_id = {ctx.guild.id}""")
+    channelid = cursor.fetchone()
+    channelid = channelid[0]
+
+    cursor.execute(f"""SELECT team FROM main WHERE guild_id = {ctx.guild.id}""")
+    team = cursor.fetchone()
+    team = team[0]
+
+    matchChannel = discord.utils.get(ctx.guild.text_channels, name=f'{channelid}')
 
     getTeams()
  
+    teamCount = 0
     counter = 0
     for teams in bteams:
         if teams == team:
+            teamCount += 1
             orangeTeam = oteams[counter]
             await matchChannel.send(f'@everyone {team} vs {orangeTeam}')
         counter += 1
@@ -124,10 +174,13 @@ async def sendmatch(ctx):
     counter = 0
     for teams in oteams:
         if teams == team:
+            teamCount += 1
             blueTeam = bteams[counter]
             await matchChannel.send(f'@everyone {team} vs {blueTeam}')
         counter += 1
 
+    if teamCount == 0:
+        await matchChannel.send("You do not have any matches!")
 # COMMAND TO CHECK WHENEVER /\
 
 
@@ -170,7 +223,7 @@ async def start(ctx):
     server_name TEXT,
     guild_id INTEGER,
     team TEXT,
-    matchChannel INTEGER    
+    channel_id TEXT    
     )
     ''')
     db.commit()
