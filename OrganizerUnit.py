487e054017802ba69417ca4448cd9e993c155054
kungfu8 <jackdilkens@gmail.com>
2020-02-19 19:09:40 -0500

    Now working with new connoisseur feature on VRML HTML!

--- OrganizerUnit.py
+++ OrganizerUnit.py
@@ -1,4 +1,4 @@
-import discord, asyncio, os, random, time, json, requests, sqlite3
+import discord, asyncio, os, random, time, json, requests, sqlite3, pickle
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from datetime import datetime
 from discord.ext import commands, tasks
@@ -8,6 +8,13 @@ token = os.environ.get('DiscordToken')
 
 client = commands.Bot(command_prefix = '.')
 client.remove_command('help')
+
+db = sqlite3.connect('discordVariables.sqlite')
+cursor = db.cursor()
+
+
+# TODO: Add Checks for Role, make sure the user has x role to use commands.
+
 
 @client.command()
 async def help(ctx):
@@ -42,17 +49,18 @@ async def info(ctx):
     teaminfo = cursor.fetchone()
     team = teaminfo[0]
 
+    cursor.execute(f"SELECT server_name FROM main WHERE guild_id = {ctx.guild.id}")
+    server_name = cursor.fetchone()
+    server_name = server_name[0]
+
     cursor.execute(f"SELECT channel_id FROM main WHERE guild_id = {ctx.guild.id}")
     matchinfo = cursor.fetchone()
     match = matchinfo[0]
-    await ctx.send(f"Team >> ***{team}***\nMatch Channel >> ***{match}***")
-# DATABASE CONNECTION FOR USE IN MULTIPLE SERVERS!
 
-
-
-db = sqlite3.connect('discordVariables.sqlite')
-cursor = db.cursor()
-
+    cursor.execute(f"SELECT role FROM main WHERE guild_id = {ctx.guild.id}")
+    role = cursor.fetchone()
+    role = role[0]
+    await ctx.send(f"**{server_name} Information:**\nTeam : `{team}`\nMatch Channel : `{match}`\nTeam Role : `{role}`")
 
 
 # Get matchChannel and Team Name and save it globally
@@ -63,6 +71,8 @@ async def setchannel(ctx, arg1):
     await client.wait_until_ready()
 
     # arg1 = int(arg1)
+    global guildID
+    global matchChannel
     matchChannel = discord.utils.get(ctx.guild.text_channels, name=f'{arg1}')
     guildID = ctx.guild.id
     mchannel = str(matchChannel)
@@ -81,10 +91,28 @@ async def setchannel(ctx, arg1):
         await matchChannel.send("Match Channel Set!")
     except:
         await ctx.send("Invalid Channel Name, Check if you wrote it correctly!")
-        print("Error SetChannel Code 0")
+        print("Error Setchannel Code 0")
 
+@client.command()
+@commands.has_permissions(administrator = True)
+async def setrole(ctx, arg1):
+    await client.wait_until_ready()
+    guildID = ctx.guild.id
+    teamrole = discord.utils.get(ctx.guild.roles, name=f'{arg1}')
+    role = str(teamrole)
+    try:
+        role_set = (f"""UPDATE main
+            SET role = ?
+            WHERE guild_id = ?
+            """)
+        info = (role, guildID)
+        cursor.execute(role_set, info)
+        db.commit()
+        print("Role Added to Database!")
+    except:
+        print("Error Adding Role to Database!")
 
-
+    await ctx.send(f"{teamrole.mention} has been set as the Role!") 
 
 @client.command()
 @commands.has_permissions(administrator = True)
@@ -140,16 +168,16 @@ async def setmatch(ctx, arg1, arg2):
 # .sremind command
 # TODO: Make it get .scrim time and remind 15 minutes before!
 @client.command()
-async def sremind(ctx, arg1, arg2, arg3):
+async def sremind(ctx, arg1, arg2):
 
     cursor.execute(f"""SELECT channel_id FROM main WHERE guild_id = {ctx.guild.id}""")
     channelid = cursor.fetchone()
     channelid = channelid[0]
 
     matchChannel = discord.utils.get(ctx.guild.text_channels, name=f'{channelid}')
-    await matchChannel.send(f"Reminder you have a scrim in >>> {arg2} {arg3}")
-    await ctx.message.delete()
 
+    await matchChannel.send(f"Reminder you have a scrim in >>> {arg1} {arg2}")
+    await ctx.message.delete()
 # Web Scrapper
 # \/
 def getTeams():
@@ -176,37 +204,45 @@ def getTeams():
 # \/
 @client.command()
 async def sendmatch(ctx):
-
-    await client.wait_until_ready()
     
+    await client.wait_until_ready()
     cursor.execute(f"""SELECT channel_id FROM main WHERE guild_id = {ctx.guild.id}""")
     channelid = cursor.fetchone()
     channelid = channelid[0]
+    print(channelid)
 
     cursor.execute(f"""SELECT team FROM main WHERE guild_id = {ctx.guild.id}""")
     team = cursor.fetchone()
     team = team[0]
+    print(team)
+
+    cursor.execute(f"SELECT role FROM main WHERE guild_id = {ctx.guild.id}")
+    role = cursor.fetchone()
+    role = role[0]
+    print(role)
 
     matchChannel = discord.utils.get(ctx.guild.text_channels, name=f'{channelid}')
+    role = discord.utils.get(ctx.guild.roles, name=f'{role}')
 
     getTeams()
- 
+    await matchChannel.send(f"{role.mention} Upcoming Matches!")
+
     teamCount = 0
     counter = 0
     for teams in bteams:
-        if teams == team:
+        if teams.startswith(f"{team}"):
             teamCount += 1
             orangeTeam = oteams[counter]
-            await matchChannel.send(f'@everyone {team} vs {orangeTeam}')
+            await matchChannel.send(f'**Blue** : {team} vs **Orange** : {orangeTeam}')
         counter += 1
 
 
     counter = 0
     for teams in oteams:
-        if teams == team:
+        if teams.startswith(f"{team}"):
             teamCount += 1
             blueTeam = bteams[counter]
-            await matchChannel.send(f'@everyone {team} vs {blueTeam}')
+            await matchChannel.send(f'**Blue** : {blueTeam} vs **Orange** : {team}')
         counter += 1
 
     if teamCount == 0:
@@ -217,26 +253,43 @@ async def sendmatch(ctx):
 # SCHEDULER EVERY MONDAY
 
 async def sendScrape():
-# 
     await client.wait_until_ready()
-    matchchannel = client.get_channel(676243741811671050)
+    cursor.execute(f"""SELECT channel_id FROM main WHERE guild_id = {guildID}""")
+    channelid = cursor.fetchone()
+    channelid = channelid[0]
 
+    cursor.execute(f"""SELECT team FROM main WHERE guild_id = {guildID}""")
+    team = cursor.fetchone()
+    team = team[0]
+
+    cursor.execute(f"SELECT role FROM main WHERE guild_id = {guildID}")
+    role = cursor.fetchone()
+    role = role[0]
+
+    matchChannel = discord.utils.get()
     getTeams()
-# 
+    await matchChannel.send(f"{role.mention} Upcoming Matches!")
+
+    teamCount = 0
     counter = 0
-    for team  in bteams:
-        if team == "Team Jokr":
+    for teams in bteams:
+        if teams.startswith(f"{team}"):
+            teamCount += 1
             orangeTeam = oteams[counter]
-            await matchchannel.send(f'@everyone Team Jokr vs {orangeTeam}')
+            await matchChannel.send(f'**Blue** : {team} vs **Orange** : {orangeTeam}')
         counter += 1
 
 
     counter = 0
-    for team in oteams:
-        if team == "Team Jokr":
+    for teams in oteams:
+        if teams.startswith(f"{team}"):
+            teamCount += 1
             blueTeam = bteams[counter]
-            await matchchannel.send(f'@everyone Team Jokr vs {blueTeam}')
+            await matchChannel.send(f'**Blue** : {blueTeam} vs **Orange** : {team}')
         counter += 1
+
+    if teamCount == 0:
+        await matchChannel.send("You do not have any matches!")
 
 
 
@@ -274,6 +327,7 @@ async def on_ready():
         server_name TEXT,
         guild_id INTEGER,
         team TEXT,
+        role TEXT,
         channel_id TEXT    
         )
         ''')
